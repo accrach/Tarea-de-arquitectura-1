4ec1490125726ccbb7ed733100f0b7bcf92948f5
accrach <mgomez@usm.cl>
2025-03-25 21:30:54 -0300

    Funcion tablero agrada

--- Tarea1.py
+++ Tarea1.py
@@ -5,14 +5,14 @@ def printeo_tablero():
     """
     Largo = int(input("Largo del pasillo que decea: "))
     i = 0
-
+    #primer while son las columnas.
     while(11>i):
         e = 0
+        #segundo while es para las filas.    
         while(Largo> e):
             print("X", end="")
             e+=1
         i+=1    
         print("")
 
-print("Hola mundo")
 printeo_tablero()
