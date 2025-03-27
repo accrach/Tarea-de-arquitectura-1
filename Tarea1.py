a6fbea7b763c462ad4ff7889357f27d39e7f2f07
martin <mpardo@usm.cl>
2025-03-27 12:18:50 -0300

    agregar guardias en tablero

--- Tarea1.py
+++ Tarea1.py
@@ -9,9 +9,9 @@ def printeo_tablero():
     guardias = int(input("Ingrese cantidad de guardias que desea: "))
 
     posiciones_guardias = random.sample(range(11 * Largo), guardias)
-    print(posiciones_guardias)
-    cant_guardias = 0
+    cant_guardias_printed = 0
     i = 0
+    contador = 0
     #primer while son las columnas.
     while(11>i):
         e = 0
@@ -20,9 +20,13 @@ def printeo_tablero():
             if e == 0 and i == 5:
                 print("S", end="")
             
+            elif cant_guardias_printed < guardias and contador in posiciones_guardias:
+                print("!", end="")
+                cant_guardias_printed += 1
             else:
                 print("X", end="")
             e+=1
+            contador += 1
         i+=1    
         print("")
 
